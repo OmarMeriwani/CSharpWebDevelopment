f57538110e1a40caf2e1f8ddaccefd432a553ca2
Omar Meriwani <omar.sirwan@gmail.com>
2017-07-20 16:16:28 +0300

    --

---
+++ MASActivationService/src/MASActivationService/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+
+namespace MASActivationService.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
