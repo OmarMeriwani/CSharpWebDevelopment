714e297b9e94803043cc9bbe9f224d790a943e30
Omar Meriwani <omar.sirwan@gmail.com>
2017-08-22 08:41:00 +0300

    ---

---
+++ MASActivationService/src/MASActivationService/Encryption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MASActivationService
+{
+    public class Encryption
+    {
+        public string encrypt(string source)
+        {
+            Chilkat.Ssh ssh = new Chilkat.Ssh();
+
+            return "";
+        }
+    }
+}
